1ea0e0f12d04ff96615a706b927852ee0113065d
ParmesanPapi17 <adammolina17@gmail.com>
2024-12-11 08:13:25 -0500

    figured out component

--- library-app/src/components/MainContainer.jsx
+++ library-app/src/components/MainContainer.jsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 function MainContainer() {
     return(
-        <div className = "bg-pale-cream m-8 p-8">
+        <div className = "bg-pale-cream m-8 p-8 h-screen">
             <div>
                 <Header />
                 <AddButton /> 
